9da07b97a5d576f738b4c5f60138129c8354d93f
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2022-11-26 16:17:25 +0200

    Change the rounding of days

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,6 +1,6 @@
 const calculateDaysBetwenDates = (date1: Date, date2: Date) => {
   const diff = Math.abs(date1.getTime() - date2?.getTime());
-  return Math.ceil(diff / (1000 * 3600 * 24));
+  return Math.floor(diff / (1000 * 3600 * 24));
 };
 
 export { calculateDaysBetwenDates };
