b968f8b69c26015dd7bf3a64ded1eb1b3823a336
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2022-12-07 22:28:51 +0200

    Finsih popup notifications and add settings page

--- MobileApp/App.xaml.cs
+++ MobileApp/App.xaml.cs
@@ -10,8 +10,18 @@ public partial class App : Application
 		InitializeComponent();
 
 		MainPage = new AppShell();
-		
+    }
+
+    protected override async void OnStart()
+    {
         OneSignalSDK.DotNet.OneSignal.Default.Initialize("ec4e9923-e2ba-47fd-871c-f0d6c6e4debd");
-        OneSignalSDK.DotNet.OneSignal.Default.PromptForPushNotificationsWithUserResponse();
+        await OneSignalSDK.DotNet.OneSignal.Default.PromptForPushNotificationsWithUserResponse();
+
+        if (!Preferences.ContainsKey("NotificationPreference"))
+        {
+            Preferences.Set("NotificationPreference", true);
+        }
+
+        OneSignalSDK.DotNet.OneSignal.Default.PushEnabled = Preferences.Get("NotificationPreference", true);
     }
 }
--- MobileApp/Pages/SettingsPage.xaml.cs
+++ MobileApp/Pages/SettingsPage.xaml.cs
@@ -5,5 +5,24 @@ public partial class SettingsPage : ContentPage
 	public SettingsPage()
 	{
 		InitializeComponent();
-	}
+        
+        var notificationPreference = Preferences.Get("NotificationPreference", true);
+
+        NotificationSwitch.IsToggled = notificationPreference;
+
+        NotificationSwitch.Toggled += (sender, e) =>
+        {
+            Preferences.Set("NotificationPreference", e.Value);
+
+            if (e.Value)
+            {
+                OneSignalSDK.DotNet.OneSignal.Default.PromptForPushNotificationsWithUserResponse();
+                OneSignalSDK.DotNet.OneSignal.Default.PushEnabled = true;
+            }
+            else
+            {
+                OneSignalSDK.DotNet.OneSignal.Default.PushEnabled = false;
+            }
+        };
+    }
 }
