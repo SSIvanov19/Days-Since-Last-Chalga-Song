567b74c6d47c21cafc2796c8c9ab76a770fd730c
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2022-12-09 01:01:52 +0200

    Add QoL improvements

--- MobileApp/App.xaml.cs
+++ MobileApp/App.xaml.cs
@@ -10,10 +10,24 @@ public partial class App : Application
 		InitializeComponent();
 
 		MainPage = new AppShell();
+        Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+    }
+
+    ~App()
+    {
+        Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
     }
 
     protected override async void OnStart()
     {
+        NetworkAccess accessType = Connectivity.Current.NetworkAccess;
+        
+        if (accessType == NetworkAccess.ConstrainedInternet || accessType != NetworkAccess.Internet)
+        {
+            await MainPage.DisplayAlert("No Internet Connection", "Please check your internet connection and try again.", "Quit App");
+            Application.Current.Quit();
+        }
+        
         OneSignalSDK.DotNet.OneSignal.Default.Initialize("ec4e9923-e2ba-47fd-871c-f0d6c6e4debd");
         await OneSignalSDK.DotNet.OneSignal.Default.PromptForPushNotificationsWithUserResponse();
 
@@ -23,5 +37,14 @@ public partial class App : Application
         }
 
         OneSignalSDK.DotNet.OneSignal.Default.PushEnabled = Preferences.Get("NotificationPreference", true);
+    }
+
+    async void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+    {
+        if (e.NetworkAccess == NetworkAccess.ConstrainedInternet || e.NetworkAccess != NetworkAccess.Internet)
+        {
+            await MainPage.DisplayAlert("No Internet Connection", "Please check your internet connection and try again.", "OK");
+            Application.Current.Quit();
+        }
     }
 }
